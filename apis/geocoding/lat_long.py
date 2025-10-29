c60bddb600f596a7a5f2a3bfb6e9f5c17a087558
Betty Bonilla <betty.bonilla04@gmail.com>
2025-10-29 15:02:58 -0400

    Add geocoding API integration

---
+++ apis/geocoding/lat_long.py
@@ -0,0 +1,64 @@
+from typing import Optional, Callable
+
+import requests
+from pydantic import BaseModel, Field
+
+_BASE_URL = "https://geocoding-api.open-meteo.com/v1/search"
+
+
+class Result(BaseModel):
+    country: str
+    country_code: str
+    state: str = Field(alias="admin1")
+    city: str = Field(alias="name")
+    # These zip codes share the same lat, long results
+    postcodes: list[str]
+    latitude: float
+    longitude: float
+    timezone: str
+
+
+class GeocodingData(BaseModel):
+    results: list[Result]
+
+
+def get_location_data(zip_code: str) -> Optional[Result]:
+    response = requests.get(
+        _BASE_URL, headers={"Accept": "application/json"}, params={"name": zip_code}
+    )
+    if response.status_code != 200:
+        return None
+
+    geocoding_data = GeocodingData(**response.json())
+    for result in geocoding_data.results:
+        if result.country_code == "US":
+            return result
+    return None
+
+
+def get_lat_long(
+    zip_code: str, location_data: Callable[[str], Optional[Result]]
+) -> dict[str, float]:
+    location_data = location_data(zip_code)
+    return {"latitude": location_data.latitude, "longitude": location_data.longitude}
+
+
+if __name__ == "__main__":
+    with open("test_data/response.json", "r") as f:
+        import json
+
+        test_data = GeocodingData(**json.load(f))
+        # print(test_data.model_dump_json(indent=4))
+        print(test_data.results[0])
+        print(test_data.results[0].state)
+        print(test_data.results[0].city)
+    print("-----------------------")
+    test_location_data = get_location_data(zip_code="07310")
+    print(test_location_data)
+    print(test_location_data.latitude)
+    print(test_location_data.longitude)
+    print("-----------------------")
+    lat_long = get_lat_long("07310", get_location_data)
+    print(lat_long)
+    print(lat_long.get("latitude"))
+    print(lat_long.get("longitude"))
